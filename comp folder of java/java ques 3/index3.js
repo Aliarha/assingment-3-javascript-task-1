9a5d5a4844c6e773add1ac16edfbc201b5bb3220
Aliarha <126072050+Aliarha@users.noreply.github.com>
2023-03-31 07:52:13 -0700

    Add files via upload

---
+++ comp folder of java/java ques 3/index3.js
@@ -0,0 +1,4 @@
+/* Declare a variable called “temperature” and assign it a value of 72. Convert the temperature to Celsius and store the result in a new variable called “celsius”. Display the value of celsius to the console.*/
+let temprature=72;
+let celcius= (temprature - 32) *5/9
+console.log(celcius);
---
+++ comp folder of java/javascript question  1/index.js
@@ -0,0 +1,5 @@
+/*Declare a variable called"age" and assingn it a value of 25. convert the value of age to a  string data type and store it in anew variable called"ageString"*/
+let age = 25;
+let ageAsString = String(age); 
+console.log("The data type of converted value of age is", typeof (ageAsString));
+    
